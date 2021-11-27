077d4cce640adc596752e6b5a53186ca3130231d
iamkathan <shethkathan4@gmail.com>
2021-11-27 19:51:51 +0000

    database half done

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Contact(models.Model):
+    fname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    cumber = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(default=timezone.now)
+    message = models.TextField()
+
+    def __str__(Self):
+        return self.email
--- Home/views.py
+++ Home/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request
+from .models import *
 
 # Create your views here.
 
 
 def home(reqeust):
-    #return HttpResponse('<h1>Kathan sheth</h1>')
     return render(reqeust, 'home/home.html')
 
 def about(request):
     return render(request, 'home/about.html')
-    #return HttpResponse('<h1>About</h1>')
     
