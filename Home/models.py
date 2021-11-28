9ce85e7d646b4ca8f2e5ea3dccfb755c521e268d
iamkathan <shethkathan4@gmail.com>
2021-11-28 02:05:30 +0000

    change has been done

--- Home/models.py
+++ Home/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Contact(models.Model):
     fname = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    email = models.CharField(max_length=100)
     cumber = models.CharField(max_length=100)
     date_posted = models.DateTimeField(default=timezone.now)
     message = models.TextField()
 
-    def __str__(Self):
+    def __str__(self):
         return self.email
--- Home/views.py
+++ Home/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request
-from .models import *
+from Home.models import Contact
 
 # Create your views here.
 
 
-def home(reqeust):
-    return render(reqeust, 'home/home.html')
+def home(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        fname = request.POST.get('fname')
+        cnumber = request.POST.get('cnumber')
+        contact = Contact(email=email, fname=fname, cnumber=cnumber)
+        contact.save()
+    return render(request, 'home/home.html')
 
 def about(request):
     return render(request, 'home/about.html')
