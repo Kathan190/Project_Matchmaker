6ddadcf01f005516c8e0beb44459489c46901eb1
iamkathan <shethkathan4@gmail.com>
2021-11-30 03:25:51 +0000

    register has been created successfully

--- Project_Matchmaker/settings.py
+++ Project_Matchmaker/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Home.apps.HomeConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
 
 ]
 
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
